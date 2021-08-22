2f33abd945bedcd9dd6c75c8419f16ab782e9dc2
Morten <morten@joorgensen.dk>
2021-08-22 21:13:13 +0200

    hygget

--- planning-site/src/components/PrioritizeTasksTab.vue
+++ planning-site/src/components/PrioritizeTasksTab.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
-    <h1>Epic</h1>
+    <h1 @click="AddTask()">Epic</h1>
+    <p>{{ this.TaskList.ListOfTasks }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { Task, TaskList } from "../types/index";
+
 export default defineComponent({
   name: "PrioritizeTasksTab",
   props: {},
+  data() {
+    return {
+      TaskList: {} as TaskList,
+    };
+  },
+  mounted(): void {
+    this.TaskList.ListOfTasks = new Array<Task>();
+  },
+
+  methods: {
+    AddTask() {
+      const test: Task = {
+        Name: "Billy",
+        Value: 5,
+      };
+      this.TaskList.ListOfTasks.push(test);
+    },
+  },
 });
 </script>
 
---
+++ planning-site/src/types/index.ts
@@ -0,0 +1,8 @@
+export interface Task {
+    Name: String;
+    Value: Number;
+}
+
+export interface TaskList {
+    ListOfTasks: Task[]
+}
